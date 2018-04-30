51a42ecc02be9f891419e87d388f77cb6fce49fc
Luis Manuel Suárez González <lsuarezg8811@gmail.com>
2018-04-30 11:19:33 +0200

    Agregando a las consultas el requisito de obtener el juego en el que se registraron los usuarios

--- scripts/usuarios_con_saldo_superior_a_X.sql
+++ scripts/usuarios_con_saldo_superior_a_X.sql
@@ -1,6 +1,11 @@
--- Obtención de los usuarios con saldo real mayor a una cantidad
--- Parametrizar el valor que voy a utilizar para comparar
+-- NOMBRE DE LA CONSULTA: Obtención de los usuarios con saldo real mayor a una cantidad
+-- SOBRE LOS RESULTADOS
+-- campo1: nombre de usuario 
+-- campo2: email del usuario
+-- campo3: saldo real del usuario
+-- campo4: nombre del juego en el que se registró el usuario
 
 SELECT myonewinner.players.username, myonewinner.users.email,
-myonewinner.players.real_balance FROM myonewinner.users INNER JOIN myonewinner.players
-where (myonewinner.users.id = myonewinner.players.users_id and myonewinner.players.real_balance > 10) Order by myonewinner.players.real_balance asc;
+myonewinner.players.real_balance, myonewinner.games.name FROM myonewinner.users INNER JOIN myonewinner.players,
+myonewinner.games
+where (myonewinner.games.id = myonewinner.players.games_register_id and myonewinner.users.id = myonewinner.players.users_id and myonewinner.players.real_balance > 10) Order by myonewinner.players.real_balance asc;
--- scripts/usuarios_sin_jugar_desde_hace_7_dias.sql
+++ scripts/usuarios_sin_jugar_desde_hace_7_dias.sql
@@ -1,6 +1,13 @@
--- Usuarios que no han jugado en los últimos 7 días
+-- Nombre de consulta: Usuarios que no han jugado en los últimos 7 ó más días
+
+-- SOBRE LOS RESULTADOS
+-- campo 1: nombre de usuario
+-- campo 2: email del usuario
+-- campo 3: último login del usuario
+-- campo 4: nombre del juego en el que se registró el usuario
+-- campo 5: nombre del último juego (Petanca o Billar) jugado por el usuario
 
 SELECT myonewinner.players.username, myonewinner.users.email, 
-myonewinner.users.last_login
-FROM myonewinner.users INNER JOIN myonewinner.players
-where (myonewinner.users.id = myonewinner.players.users_id and (datediff(now(),myonewinner.users.last_login)=7)) Order by myonewinner.users.last_login desc;
+myonewinner.users.last_login, myonewinner.games.name
+FROM myonewinner.users INNER JOIN myonewinner.players, myonewinner.games
+where (myonewinner.players.games_register_id = myonewinner.games.id and myonewinner.users.id = myonewinner.players.users_id and (datediff(now(),myonewinner.users.last_login)>=7)) Order by myonewinner.users.last_login desc;
